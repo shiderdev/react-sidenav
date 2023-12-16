96f4e746069d3e289d374f4d311a54b045719e78
proton <proton@proton>
2023-12-16 16:17:59 +0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: {
+      import: "index.jsx",
+    },
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: ["node_modules", "src"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./build",
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
+  },
+  target: "web",
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      chunks: ["main"],
+      publicPath: "/",
+      template: "assets/index.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+    publicPath: "",
+  },
+};
